0ba04b3cfa901ebe6fb704387f5808383febce6c
Songmu <y.songmu@gmail.com>
2018-01-07 23:34:01 +0900

    update

--- goxz.rb
+++ goxz.rb
@@ -4,20 +4,9 @@ class Goxz < Formula
   homepage "https://github.com/Songmu/#{@@name}"
   url "https://github.com/Songmu/#{@@name}/releases/download/v#{version}/#{@@name}_v#{version}_darwin_amd64.zip"
   sha256 '5b4cdb7b0862484114d9ef21dd5dc016c194c2314d13e2a129f9ed5fbe01f411'
-
-  head do
-    url "https://github.com/Songmu/#{@@name}.git"
-    depends_on 'go' => :build
-  end
+  head "https://github.com/Songmu/#{@@name}.git"
 
   def install
-    if build.head?
-      ENV['GOPATH'] = buildpath
-      mkdir_p buildpath/'src/github.com/Songmu'
-      ln_s buildpath, buildpath/"src/github.com/Songmu/#{@@name}"
-      system 'make', 'deps'
-      system 'make', 'build'
-    end
     bin.install @@name
   end
 end
