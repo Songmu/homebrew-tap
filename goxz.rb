bd564ec7d8d0f0d8e6d6d11c20e023dae835e805
Songmu <y.songmu@gmail.com>
2018-01-07 23:09:19 +0900

    update goxz

--- goxz.rb
+++ goxz.rb
@@ -1,12 +1,12 @@
-HOMEBREW_GOXZ_VERSION='0.0.1'
 class Goxz < Formula
-  homepage 'https://github.com/Songmu/goxz'
-  version HOMEBREW_GOXZ_VERSION
-  url "https://github.com/Songmu/goxz/releases/download/v#{HOMEBREW_GOXZ_VERSION}/goxz_v#{HOMEBREW_GOXZ_VERSION}_darwin_amd64.zip"
-  sha256 'dda16c446125e476f972ee2a629f75d5e8a23dc41429b3c57c38d2c0601bcf1a'
+  version '0.0.2'
+  name 'goxz'
+  homepage "https://github.com/Songmu/#{name}"
+  url "https://github.com/Songmu/#{name}/releases/download/v#{version}/#{name}_v#{version}_darwin_amd64.zip"
+  sha256 '5b4cdb7b0862484114d9ef21dd5dc016c194c2314d13e2a129f9ed5fbe01f411'
 
   head do
-    url 'https://github.com/Songmu/goxz.git'
+    url "https://github.com/Songmu/#{name}.git"
     depends_on 'go' => :build
     depends_on 'hg' => :build
   end
@@ -15,14 +15,10 @@ class Goxz < Formula
     if build.head?
       ENV['GOPATH'] = buildpath
       mkdir_p buildpath/'src/github.com/Songmu'
-      ln_s buildpath, buildpath/'src/github.com/Songmu/goxz'
-      system 'go', 'get', '-d', '-v', '.'
-      system 'go', 'build', '-o', 'goxz', './cmd/goxz/'
+      ln_s buildpath, buildpath/"src/github.com/Songmu/#{name}"
+      system 'make', 'deps'
+      system 'make', 'build'
     end
-    bin.install 'goxz'
-  end
-
-  test do
-    system 'goxz', 'version'
+    bin.install name
   end
 end
