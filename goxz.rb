6914fecdabf73b4c3838e051cfedc6b4f71d58c2
Songmu <y.songmu@gmail.com>
2018-01-08 00:50:28 +0900

    Revert "name"
    This reverts commit ea68e0a4d557cd0a3520b029cbe2b725815a7896.

--- goxz.rb
+++ goxz.rb
@@ -1,11 +1,12 @@
 class Goxz < Formula
+  @@name = 'goxz'
   version '0.0.2'
-  homepage "https://github.com/Songmu/#{@name}"
-  url "https://github.com/Songmu/#{@name}/releases/download/v#{version}/#{@name}_v#{version}_darwin_amd64.zip"
+  homepage "https://github.com/Songmu/#{@@name}"
+  url "https://github.com/Songmu/#{@@name}/releases/download/v#{version}/#{@@name}_v#{version}_darwin_amd64.zip"
   sha256 '5b4cdb7b0862484114d9ef21dd5dc016c194c2314d13e2a129f9ed5fbe01f411'
 
   head do
-    url "https://github.com/Songmu/#{@name}.git"
+    url "https://github.com/Songmu/#{@@name}.git"
     depends_on 'go' => :build
   end
 
@@ -17,11 +18,11 @@ class Goxz < Formula
         'PATH'   => "#{gobin}:#{ENV['PATH']}",
       })
       mkdir_p buildpath/'src/github.com/Songmu'
-      ln_s buildpath, buildpath/"src/github.com/Songmu/#{@name}"
+      ln_s buildpath, buildpath/"src/github.com/Songmu/#{@@name}"
       system 'go', 'get', '-d', '-v', './...'
-      system 'go', 'build', "./cmd/#{@name}"
+      system 'go', 'build', "./cmd/#{@@name}"
     end
 
-    bin.install @name
+    bin.install @@name
   end
 end
