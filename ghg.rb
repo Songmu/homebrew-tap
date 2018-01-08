36a4981d2bbf6ae56e06e413db50676bb1f413de
Songmu <y.songmu@gmail.com>
2018-01-08 22:46:52 +0900

    adjust ghg.rb

--- ghg.rb
+++ ghg.rb
@@ -1,14 +1,13 @@
-HOMEBREW_GHG_VERSION='0.1.0'
 class Ghg < Formula
+
+  version '0.1.0'
   homepage 'https://github.com/Songmu/ghg'
-  version HOMEBREW_GHG_VERSION
-  url "https://github.com/Songmu/ghg/releases/download/v#{HOMEBREW_GHG_VERSION}/ghg_v#{HOMEBREW_GHG_VERSION}_darwin_amd64.zip"
+  url "https://github.com/Songmu/ghg/releases/download/v#{version}/ghg_v#{version}_darwin_amd64.zip"
   sha256 'd50e40799e43f5c1b1b9a651ad5c30df66d32af78a5280b32f8c12b54e7cf2c2'
 
   head do
     url 'https://github.com/Songmu/ghg.git'
     depends_on 'go' => :build
-    depends_on 'hg' => :build
   end
 
   def install
@@ -17,12 +16,8 @@ class Ghg < Formula
       mkdir_p buildpath/'src/github.com/Songmu'
       ln_s buildpath, buildpath/'src/github.com/Songmu/ghg'
       system 'go', 'get', '-d', '-v', '.'
-      system 'go', 'build', '-o', 'ghg', './cmd/ghg/'
+      system 'go', 'build', './cmd/ghg'
     end
     bin.install 'ghg'
-  end
-
-  test do
-    system 'ghg', 'version'
   end
 end
