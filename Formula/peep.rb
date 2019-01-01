915b9f98fb36a86197f9ad6f36af62571de46e38
Songmu <y.songmu@gmail.com>
2019-01-01 23:37:42 +0900

    update peep.rb

--- Formula/peep.rb
+++ Formula/peep.rb
@@ -1,8 +1,8 @@
 class Peep < Formula
-  version '0.0.2'
+  version '0.1.0'
   homepage 'https://github.com/Songmu/peep'
-  url "https://github.com/Songmu/peep/releases/download/v0.0.2/peep_v0.0.2_darwin_amd64.zip"
-  sha256 '496e2771273afab91b28bcede78c9010531f01ad853221be49f9ca2e1c52407a'
+  url "https://github.com/Songmu/peep/releases/download/v0.1.0/peep_v0.1.0_darwin_amd64.zip"
+  sha256 '0ac2ab34b3dd4feb65a032d1d70f033cd1e0ab8639de18842373ed8e454686f1'
   head 'https://github.com/Songmu/peep.git'
 
   def install
