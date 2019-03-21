f05e558c080fc9c0d744c16607599a3c5bbd009d
Songmu <y.songmu@gmail.com>
2019-03-21 22:27:10 +0900

    update godzil

--- Formula/godzil.rb
+++ Formula/godzil.rb
@@ -1,8 +1,8 @@
 class Godzil < Formula
-  version '0.1.3'
+  version '0.2.0'
   homepage 'https://github.com/Songmu/godzil'
-  url "https://github.com/Songmu/godzil/releases/download/v0.1.3/godzil_v0.1.3_darwin_amd64.zip"
-  sha256 '1694e1473b75c14a19191f37d97c410e636977a610f693803ba0d72690e9c762'
+  url "https://github.com/Songmu/godzil/releases/download/v0.2.0/godzil_v0.2.0_darwin_amd64.zip"
+  sha256 '1ab72ec601a7f53038987245a36d3fd0950b28d9d2ed0f19aa08a7e8a0092930'
   head 'https://github.com/Songmu/godzil.git'
 
   def install
