067f984d3aa515ea0ce4b4d4150355359ba0eb2c
Songmu <y.songmu@gmail.com>
2019-04-16 15:07:43 +0900

    update gocredits

--- Formula/gocredits.rb
+++ Formula/gocredits.rb
@@ -1,8 +1,8 @@
 class Gocredits < Formula
-  version '0.0.3'
+  version '0.0.4'
   homepage 'https://github.com/Songmu/gocredits'
-  url "https://github.com/Songmu/gocredits/releases/download/v0.0.3/gocredits_v0.0.3_darwin_amd64.zip"
-  sha256 '0ccc4c961c71424d9222a2403c560db6e98e4bb0fb8a8a29f6eda39c25b210ca'
+  url "https://github.com/Songmu/gocredits/releases/download/v0.0.4/gocredits_v0.0.4_darwin_amd64.zip"
+  sha256 '636809cdc2c85ea5d1bba6d87ee2ae7284345648d06596746e64f3681750ea62'
   head 'https://github.com/Songmu/gocredits.git'
 
   head do
