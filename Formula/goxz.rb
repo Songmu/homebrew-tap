2aaf73d8e6413d43b8babac1bb5bcc5b1f5926ce
Songmu <y.songmu@gmail.com>
2019-05-01 13:38:31 +0900

    update goxz

--- Formula/goxz.rb
+++ Formula/goxz.rb
@@ -1,9 +1,9 @@
 class Goxz < Formula
   name = 'goxz'
-  version '0.4.0'
+  version '0.4.1'
   homepage "https://github.com/Songmu/#{name}"
   url "https://github.com/Songmu/#{name}/releases/download/v#{version}/#{name}_v#{version}_darwin_amd64.zip"
-  sha256 '4f803299001bd0cc00f3bcfff723f90ab79801e620b9344d9f586ed55df1380a'
+  sha256 'ee35ce73e73e428694b49492720c7ec6b9e90195cef640e10bd819058a7da5ea'
 
   head do
     url "https://github.com/Songmu/#{name}.git"
