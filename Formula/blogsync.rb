f8a4693c0c830ade340058f0d51e37b601ae4f93
Songmu <y.songmu@gmail.com>
2020-01-12 01:39:13 +0900

    update godzil and blogsync

--- Formula/blogsync.rb
+++ Formula/blogsync.rb
@@ -1,8 +1,8 @@
 class Blogsync < Formula
-  version '0.10.2'
+  version '0.10.3'
   homepage 'https://github.com/motemen/blogsync'
-  url "https://github.com/motemen/blogsync/releases/download/v0.10.2/blogsync_v0.10.2_darwin_amd64.zip"
-  sha256 'f66ac4ab5da17614cd5f505763ff17be0056818fd255153ec2ab1c38d3a7fb45'
+  url "https://github.com/motemen/blogsync/releases/download/v0.10.3/blogsync_v0.10.3_darwin_amd64.zip"
+  sha256 'f39cdeaf83f400f0e2ac295aacd4bc97b2b2b196a7d869bd4c41cdd2fa1b428d'
   head 'https://github.com/motemen/blogsync.git'
 
   def install
--- Formula/godzil.rb
+++ Formula/godzil.rb
@@ -1,8 +1,8 @@
 class Godzil < Formula
-  version '0.8.0'
+  version '0.8.2'
   homepage 'https://github.com/Songmu/godzil'
-  url "https://github.com/Songmu/godzil/releases/download/v0.8.0/godzil_v0.8.0_darwin_amd64.zip"
-  sha256 'af997b7b2dd549dbe94cb578333064bddaad07abdcd4e07f56937960540e276d'
+  url "https://github.com/Songmu/godzil/releases/download/v0.8.2/godzil_v0.8.2_darwin_amd64.zip"
+  sha256 '61b188c2ac054484cfc1fd75a6f242e54a623080b5b068d914b5310a01f1d92f'
   head 'https://github.com/Songmu/godzil.git'
 
   def install
