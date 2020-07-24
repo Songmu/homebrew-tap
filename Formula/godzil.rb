cfb42dc4d27a83552d48d0f0ff258d80d11ed797
Songmu <y.songmu@gmail.com>
2020-07-24 19:53:13 +0900

    update godzil and kibelasync

--- Formula/godzil.rb
+++ Formula/godzil.rb
@@ -1,8 +1,8 @@
 class Godzil < Formula
-  version '0.8.5'
+  version '0.9.0'
   homepage 'https://github.com/Songmu/godzil'
-  url "https://github.com/Songmu/godzil/releases/download/v0.8.5/godzil_v0.8.5_darwin_amd64.zip"
-  sha256 '32c1839e7ef885796424908c3f61331c8c6011296c9f2bb5655e98a308cb6131'
+  url "https://github.com/Songmu/godzil/releases/download/v0.9.0/godzil_v0.9.0_darwin_amd64.zip"
+  sha256 '56507733e8e3656905226bcf6e221ebc07d17fec5549b710761fe89de2f19c5b'
   head 'https://github.com/Songmu/godzil.git'
 
   def install
--- Formula/kibelasync.rb
+++ Formula/kibelasync.rb
@@ -1,8 +1,8 @@
 class Kibelasync < Formula
-  version '0.0.4'
+  version '0.0.7'
   homepage 'https://github.com/Songmu/kibelasync'
-  url "https://github.com/Songmu/kibelasync/releases/download/v0.0.4/kibelasync_v0.0.4_darwin_amd64.zip"
-  sha256 'dfbda77e33b8d2cf3bb2bc2f52fffd97aec2d9c7921f8968a81af363c115804d'
+  url "https://github.com/Songmu/kibelasync/releases/download/v0.0.7/kibelasync_v0.0.7_darwin_amd64.zip"
+  sha256 '5e0a59ee61c2de85fc70af3c804f1f9e5bd510932042eba8402100548380b589'
   head 'https://github.com/Songmu/kibelasync.git'
 
   head do
