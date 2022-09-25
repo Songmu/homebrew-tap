cdc388b40878c7bf7434b8fe6e54f7167c7dc344
Songmu <y.songmu@gmail.com>
2022-09-25 21:33:46 +0900

    updat maltmill

--- Formula/maltmill.rb
+++ Formula/maltmill.rb
@@ -1,8 +1,8 @@
 class Maltmill < Formula
-  version '1.0.0'
+  version '1.0.1'
   homepage 'https://github.com/Songmu/maltmill'
-  url "https://github.com/Songmu/maltmill/releases/download/v1.0.0/maltmill_v1.0.0_darwin_amd64.zip"
-  sha256 'df4431d124d8912d8916b8e05fe5e27b9752a5e261fe8283f9b37fc3a5ac3767'
+  url "https://github.com/Songmu/maltmill/releases/download/v1.0.1/maltmill_v1.0.1_darwin_amd64.zip"
+  sha256 '205a6f029aff401897e2f352d6512ac8ebe6e210863395917eb0b6273c022c66'
   head 'https://github.com/Songmu/maltmill.git'
 
   head do
