4eb2d4a45e5e6930a66454751c541c828f7d2fd0
Songmu <y.songmu@gmail.com>
2022-12-23 09:31:39 +0000

    update to newer version

--- Formula/ecschedule.rb
+++ Formula/ecschedule.rb
@@ -1,26 +1,26 @@
 class Ecschedule < Formula
-  version '0.7.1'
+  version '0.7.2'
   homepage 'https://github.com/Songmu/ecschedule'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/Songmu/ecschedule/releases/download/v0.7.1/ecschedule_v0.7.1_darwin_arm64.zip'
-      sha256 '5e4dde3a39f8a9c18297df864227df73869f40b5eecd1afcfc7d4dc565b5fc7e'
+      url 'https://github.com/Songmu/ecschedule/releases/download/v0.7.2/ecschedule_v0.7.2_darwin_arm64.zip'
+      sha256 'cca019e32dfdc019085a03a98a7cde29f6fcedfe0d269433de9ec3795223325a'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/Songmu/ecschedule/releases/download/v0.7.1/ecschedule_v0.7.1_darwin_amd64.zip'
-      sha256 'dfd46e669e57e8dd739e0ae8ae01faa8962ac1a964c56a550a9e1107a56946dd'
+      url 'https://github.com/Songmu/ecschedule/releases/download/v0.7.2/ecschedule_v0.7.2_darwin_amd64.zip'
+      sha256 'dbd82af32692e6f1724758a04c5f7615ce868fb36ca6fb67a9574683f9b5d0bc'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/Songmu/ecschedule/releases/download/v0.7.1/ecschedule_v0.7.1_linux_arm64.tar.gz'
-      sha256 '93f0b8488a30049fe734847225009924fd85614e8a223cf7ff0259b72991628f'
+      url 'https://github.com/Songmu/ecschedule/releases/download/v0.7.2/ecschedule_v0.7.2_linux_arm64.tar.gz'
+      sha256 '6afb88dab4fadd8644cfaf0cdbfa5a505bf6f7ac339190d43eae866a024d9022'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/Songmu/ecschedule/releases/download/v0.7.1/ecschedule_v0.7.1_linux_amd64.tar.gz'
-      sha256 '4b820bb9aded2c516c6e7afa87d9f3fda4ad4d3405669b08b1759da7a02a0af7'
+      url 'https://github.com/Songmu/ecschedule/releases/download/v0.7.2/ecschedule_v0.7.2_linux_amd64.tar.gz'
+      sha256 'a6921f4b99e8230507549a421b7b7018fa789477a361aaa79a13e40982eddd82'
     end
   end
 
