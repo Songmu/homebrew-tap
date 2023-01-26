958327c55ef50df295edc67a1ebd2d92a0730fbf
Songmu <y.songmu@gmail.com>
2023-01-26 00:45:59 +0000

    update to newer version

--- Formula/blogsync.rb
+++ Formula/blogsync.rb
@@ -1,8 +1,8 @@
 class Blogsync < Formula
-  version '0.12.3'
+  version '0.13.0'
   homepage 'https://github.com/motemen/blogsync'
-  url "https://github.com/x-motemen/blogsync/releases/download/v0.12.3/blogsync_v0.12.3_darwin_amd64.zip"
-  sha256 '0f714449f1eeaf982c4f1802f2204e6b622bee3e0d3b45ea5370c01062d1d7e5'
+  url "https://github.com/x-motemen/blogsync/releases/download/v0.13.0/blogsync_v0.13.0_darwin_amd64.zip"
+  sha256 'fab44a3945d4e4e179aa09a7d146f2a36ace058d639f2e6942915780c9baa72c'
   head 'https://github.com/motemen/blogsync.git'
 
   def install
