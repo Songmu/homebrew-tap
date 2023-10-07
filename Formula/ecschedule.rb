40d4d77fc6ea41952cb486cbe5852e2c8a3fcbbc
Songmu <y.songmu@gmail.com>
2023-10-07 00:43:37 +0000

    update to newer version

--- Formula/ecschedule.rb
+++ Formula/ecschedule.rb
@@ -1,26 +1,26 @@
 class Ecschedule < Formula
-  version '0.10.2'
+  version '0.10.3'
   homepage 'https://github.com/Songmu/ecschedule'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/Songmu/ecschedule/releases/download/v0.10.2/ecschedule_v0.10.2_darwin_arm64.zip'
-      sha256 '97be75790b49d990334946a975483e9ea5d4a22321bcf0b4cdc75b799d7d3068'
+      url 'https://github.com/Songmu/ecschedule/releases/download/v0.10.3/ecschedule_v0.10.3_darwin_arm64.zip'
+      sha256 '78fcfea9c4e74f3467c14346712d55f866196ae338d29797391b45b3a4e257ef'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/Songmu/ecschedule/releases/download/v0.10.2/ecschedule_v0.10.2_darwin_amd64.zip'
-      sha256 'b3cd94b113d1fe9550fcae016e1674befbbe7a8a0a6262ffb6c1cb03acc76caf'
+      url 'https://github.com/Songmu/ecschedule/releases/download/v0.10.3/ecschedule_v0.10.3_darwin_amd64.zip'
+      sha256 '00dbd4541a923a58e69b2cc7c6a0f9b2ecfeea6d9d87581413990fcdc110995d'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/Songmu/ecschedule/releases/download/v0.10.2/ecschedule_v0.10.2_linux_arm64.tar.gz'
-      sha256 '206f49e86c27170c092ffd3389a08881c027923889c5f31f14171065e7743c5d'
+      url 'https://github.com/Songmu/ecschedule/releases/download/v0.10.3/ecschedule_v0.10.3_linux_arm64.tar.gz'
+      sha256 '2e01fa9c41fb6ac05fdddd1a2d388c96b8c9495c98401bdd9f910ca6445fc7bb'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/Songmu/ecschedule/releases/download/v0.10.2/ecschedule_v0.10.2_linux_amd64.tar.gz'
-      sha256 '5c3230387442ab097912a74f020288eb8e52554ff75e1205cd48881aa6d66876'
+      url 'https://github.com/Songmu/ecschedule/releases/download/v0.10.3/ecschedule_v0.10.3_linux_amd64.tar.gz'
+      sha256 '57abd7b4690db0323e7a264eed98dbfcb477a3c66faed110df0531ea40f59a64'
     end
   end
 
