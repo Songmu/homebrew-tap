10d75ee04749b535fd11439ca8ab0e695f85104a
Songmu <y.songmu@gmail.com>
2024-01-01 00:43:59 +0000

    update to newer version

--- Formula/gh2changelog.rb
+++ Formula/gh2changelog.rb
@@ -1,26 +1,26 @@
 class Gh2changelog < Formula
-  version '0.0.4'
+  version '0.0.5'
   homepage 'https://github.com/Songmu/gh2changelog'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/Songmu/gh2changelog/releases/download/v0.0.4/gh2changelog_v0.0.4_darwin_arm64.zip'
-      sha256 'de9efc8914ea365c138a001dee4a51b633cf054952f997531d6f9ef4dd18e879'
+      url 'https://github.com/Songmu/gh2changelog/releases/download/v0.0.5/gh2changelog_v0.0.5_darwin_arm64.zip'
+      sha256 '71aa471aa1ddb2f4fa7c5da743e6551b912a4ab8bd76a48478103bf763e83026'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/Songmu/gh2changelog/releases/download/v0.0.4/gh2changelog_v0.0.4_darwin_amd64.zip'
-      sha256 'ee6d74e533c59e86756ce083f2023bf253de63363e386f4c6357a18404ca0e87'
+      url 'https://github.com/Songmu/gh2changelog/releases/download/v0.0.5/gh2changelog_v0.0.5_darwin_amd64.zip'
+      sha256 '6f3a066845ddf43650a5e3f9e111467cc74ea004b6306e667d69da5be8dd001a'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/Songmu/gh2changelog/releases/download/v0.0.4/gh2changelog_v0.0.4_linux_arm64.tar.gz'
-      sha256 'f2c96fea1a391e952b7050a5b8fe7c558b865cb13fb68adb7281a9eb0253ea90'
+      url 'https://github.com/Songmu/gh2changelog/releases/download/v0.0.5/gh2changelog_v0.0.5_linux_arm64.tar.gz'
+      sha256 'f9b376d17152af567273538639d720b2e7ef0ee6be1d867b9d32da3f75b12640'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/Songmu/gh2changelog/releases/download/v0.0.4/gh2changelog_v0.0.4_linux_amd64.tar.gz'
-      sha256 'afd51e9f4ad3fae01bed1bf95414b031f0e7eb98477760f5e34bd4c5b7cd9014'
+      url 'https://github.com/Songmu/gh2changelog/releases/download/v0.0.5/gh2changelog_v0.0.5_linux_amd64.tar.gz'
+      sha256 '67f6fdffcc5348a7194998dc65708a6874eceacc079db72297817568668ff406'
     end
   end
 
