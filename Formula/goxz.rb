bc62b2cca4b64367450f1ff50cabddfc17c68c7d
Songmu <y.songmu@gmail.com>
2025-06-19 00:47:41 +0000

    update to newer version

--- Formula/goxz.rb
+++ Formula/goxz.rb
@@ -1,26 +1,26 @@
 class Goxz < Formula
-  version '0.9.1'
+  version '0.10.1'
   homepage 'https://github.com/Songmu/goxz'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/Songmu/goxz/releases/download/v0.9.1/goxz_v0.9.1_darwin_arm64.zip'
-      sha256 '9b78c0bdfdab4bed18e09c553651b8c053e2722fbbf1cff38e478d8ab1434598'
+      url 'https://github.com/Songmu/goxz/releases/download/v0.10.1/goxz_v0.10.1_darwin_arm64.zip'
+      sha256 'a980a03edd3f2143cf4e24d06ebc71cbf51208dff3facc91bb3dafb7445ec44b'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/Songmu/goxz/releases/download/v0.9.1/goxz_v0.9.1_darwin_amd64.zip'
-      sha256 '3d784bc4e1182a3f30b421bcc862b29a536463ec05f745b2b657ad7840b26216'
+      url 'https://github.com/Songmu/goxz/releases/download/v0.10.1/goxz_v0.10.1_darwin_amd64.zip'
+      sha256 'cbfc5da6993b29026f88e4bdf4eea5ed3db0fbe6e1c8da7f4afe97cfd166469e'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/Songmu/goxz/releases/download/v0.9.1/goxz_v0.9.1_linux_arm64.tar.gz'
-      sha256 '3950e8cdb3d48c858f78e9819267c2ce0fdea1b1d9d05c5341827587973dc5bb'
+      url 'https://github.com/Songmu/goxz/releases/download/v0.10.1/goxz_v0.10.1_linux_arm64.tar.gz'
+      sha256 '8c2043755b6a5389ba98164ee123f1ece5a1bfe6a1cff2c34b2295f98fab665f'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/Songmu/goxz/releases/download/v0.9.1/goxz_v0.9.1_linux_amd64.tar.gz'
-      sha256 'dfd633bc5799dcb92a4cb25fb9c6e36c2c92426362f3d45998a267eac117ce72'
+      url 'https://github.com/Songmu/goxz/releases/download/v0.10.1/goxz_v0.10.1_linux_amd64.tar.gz'
+      sha256 '7500bba6c089fa54a5b7b2b123239dcda128cff1b18ef230535b295e1980015c'
     end
   end
 
