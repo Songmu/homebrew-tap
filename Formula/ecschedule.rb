5452c97bee0d7bf51e1a779ed5388c4965637deb
Songmu <y.songmu@gmail.com>
2025-07-01 00:48:03 +0000

    update to newer version

--- Formula/ecschedule.rb
+++ Formula/ecschedule.rb
@@ -1,26 +1,26 @@
 class Ecschedule < Formula
-  version '0.14.0'
+  version '0.15.0'
   homepage 'https://github.com/Songmu/ecschedule'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/Songmu/ecschedule/releases/download/v0.14.0/ecschedule_v0.14.0_darwin_arm64.zip'
-      sha256 '381cef3e77b060344f2d097b7cc81bf2debd3d61c0bfd29dca296348c653d526'
+      url 'https://github.com/Songmu/ecschedule/releases/download/v0.15.0/ecschedule_v0.15.0_darwin_arm64.zip'
+      sha256 '3dcf86029d65c4f17a0583f91e0e9167f2b2a6d056620d31c20d1e3cc2c20ff2'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/Songmu/ecschedule/releases/download/v0.14.0/ecschedule_v0.14.0_darwin_amd64.zip'
-      sha256 '50a50118b4d57a5f42554a48f9a09c99cfa5fb7eac3fc570099082808d5b5148'
+      url 'https://github.com/Songmu/ecschedule/releases/download/v0.15.0/ecschedule_v0.15.0_darwin_amd64.zip'
+      sha256 '9c5d58193b84c3df222fe20a22307d21cb5b098ed9328ab23d6693ad54db4e96'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/Songmu/ecschedule/releases/download/v0.14.0/ecschedule_v0.14.0_linux_arm64.tar.gz'
-      sha256 'efd4584687bb30cc32b61bfd177fcefc5bbab499256d55066270946eb7da01ea'
+      url 'https://github.com/Songmu/ecschedule/releases/download/v0.15.0/ecschedule_v0.15.0_linux_arm64.tar.gz'
+      sha256 '8cc3f447cc59c6d22ec0e7f539a7a99fb84ab20e3e6855ce7f520618aa7a128d'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/Songmu/ecschedule/releases/download/v0.14.0/ecschedule_v0.14.0_linux_amd64.tar.gz'
-      sha256 '8a4f50566ececb847fd4b5f1ed0e6b7319ea749572a0959ccfcae5263b27760a'
+      url 'https://github.com/Songmu/ecschedule/releases/download/v0.15.0/ecschedule_v0.15.0_linux_amd64.tar.gz'
+      sha256 'eadba81bc1e193a13808e87eee6c0c7b1bf141d7d10e84581e1ef5d2d85f5e74'
     end
   end
 
