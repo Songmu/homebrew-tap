9de814aad2c0063189cf261dbb9a6652a99e82f9
Songmu <y.songmu@gmail.com>
2025-08-01 09:39:50 +0000

    update to newer version

--- Formula/ecschedule.rb
+++ Formula/ecschedule.rb
@@ -1,26 +1,26 @@
 class Ecschedule < Formula
-  version '0.15.0'
+  version '0.15.1'
   homepage 'https://github.com/Songmu/ecschedule'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/Songmu/ecschedule/releases/download/v0.15.0/ecschedule_v0.15.0_darwin_arm64.zip'
-      sha256 '3dcf86029d65c4f17a0583f91e0e9167f2b2a6d056620d31c20d1e3cc2c20ff2'
+      url 'https://github.com/Songmu/ecschedule/releases/download/v0.15.1/ecschedule_v0.15.1_darwin_arm64.zip'
+      sha256 '5e26c6d990abc0ab097382fddda55a50e1a1bf4b7b59bbabfa04da4d6477c5b6'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/Songmu/ecschedule/releases/download/v0.15.0/ecschedule_v0.15.0_darwin_amd64.zip'
-      sha256 '9c5d58193b84c3df222fe20a22307d21cb5b098ed9328ab23d6693ad54db4e96'
+      url 'https://github.com/Songmu/ecschedule/releases/download/v0.15.1/ecschedule_v0.15.1_darwin_amd64.zip'
+      sha256 '384a218b287b2af431924408fc11684f16fde155723a63f803b9a5ff84e82189'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/Songmu/ecschedule/releases/download/v0.15.0/ecschedule_v0.15.0_linux_arm64.tar.gz'
-      sha256 '8cc3f447cc59c6d22ec0e7f539a7a99fb84ab20e3e6855ce7f520618aa7a128d'
+      url 'https://github.com/Songmu/ecschedule/releases/download/v0.15.1/ecschedule_v0.15.1_linux_arm64.tar.gz'
+      sha256 'fda6c08556fa451a3bf1241657d5a2ce64d0afc74ffd10cbc1e0332ac509539f'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/Songmu/ecschedule/releases/download/v0.15.0/ecschedule_v0.15.0_linux_amd64.tar.gz'
-      sha256 'eadba81bc1e193a13808e87eee6c0c7b1bf141d7d10e84581e1ef5d2d85f5e74'
+      url 'https://github.com/Songmu/ecschedule/releases/download/v0.15.1/ecschedule_v0.15.1_linux_amd64.tar.gz'
+      sha256 '05a6702524cef3ddfd952263e38cb02896d43ab367d4ae1828c370dbada284f3'
     end
   end
 
