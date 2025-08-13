cb606d691d356beaf2560ef55d580721ef834d41
Songmu <y.songmu@gmail.com>
2025-08-13 13:50:10 +0900

    update laminate

--- Formula/laminate.rb
+++ Formula/laminate.rb
@@ -1,27 +1,27 @@
 class Laminate < Formula
   desc 'A command-line bridge tool that orchestrates external image generation commands to convert text/code strings to images.'
-  version '0.0.1'
+  version '0.0.2'
   homepage 'https://github.com/Songmu/laminate'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/Songmu/laminate/releases/download/v0.0.1/laminate_v0.0.1_darwin_arm64.zip'
-      sha256 '5ea24ce26d501b7c31cbf9d861b63ac5c90bb300a57ab61ebdfca5bd8de861c1'
+      url 'https://github.com/Songmu/laminate/releases/download/v0.0.2/laminate_v0.0.2_darwin_arm64.zip'
+      sha256 '4aafaa9773fdae6932c13e9d3014905148109874d118ce8b730e733a4ca71a5b'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/Songmu/laminate/releases/download/v0.0.1/laminate_v0.0.1_darwin_amd64.zip'
-      sha256 '73dd02777a1cdfababef6f4133a8f11db6733eefe3f7f0b361ca8cd8169c5e9f'
+      url 'https://github.com/Songmu/laminate/releases/download/v0.0.2/laminate_v0.0.2_darwin_amd64.zip'
+      sha256 '1bd9550d39ca84504e4d6c2394bcadf84f2a63132ee8a279f964609a80b38011'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/Songmu/laminate/releases/download/v0.0.1/laminate_v0.0.1_linux_arm64.tar.gz'
-      sha256 '492f173b7ff9ebc151bd5ca77414131401b242c53774e7a90d355fdcb3911b65'
+      url 'https://github.com/Songmu/laminate/releases/download/v0.0.2/laminate_v0.0.2_linux_arm64.tar.gz'
+      sha256 '0f011cdb07547a5a1ff16e934b851ce38c9885af53c6de8764db2c623b20ad07'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/Songmu/laminate/releases/download/v0.0.1/laminate_v0.0.1_linux_amd64.tar.gz'
-      sha256 '63615c3d0ceb2f5fe7da8caeeef85c54257cf48c88ca0eaff20565f045008f89'
+      url 'https://github.com/Songmu/laminate/releases/download/v0.0.2/laminate_v0.0.2_linux_amd64.tar.gz'
+      sha256 '9c22d968b0ddb9667f37e6bf3a0f4864f25c86028992e597921ae29c02441ced'
     end
   end
 
