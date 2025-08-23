d251b49912a62578ec0b43196ad5fe55539b55e4
Songmu <y.songmu@gmail.com>
2025-08-23 00:47:24 +0000

    update to newer version

--- Formula/laminate.rb
+++ Formula/laminate.rb
@@ -1,27 +1,27 @@
 class Laminate < Formula
   desc 'A command-line bridge tool that orchestrates external image generation commands to convert text/code strings to images.'
-  version '0.0.2'
+  version '0.0.3'
   homepage 'https://github.com/Songmu/laminate'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/Songmu/laminate/releases/download/v0.0.2/laminate_v0.0.2_darwin_arm64.zip'
-      sha256 '4aafaa9773fdae6932c13e9d3014905148109874d118ce8b730e733a4ca71a5b'
+      url 'https://github.com/Songmu/laminate/releases/download/v0.0.3/laminate_v0.0.3_darwin_arm64.zip'
+      sha256 'c1051eba0998f18d25c418420e485d52dfd7499117e227f14a7fec87062becf7'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/Songmu/laminate/releases/download/v0.0.2/laminate_v0.0.2_darwin_amd64.zip'
-      sha256 '1bd9550d39ca84504e4d6c2394bcadf84f2a63132ee8a279f964609a80b38011'
+      url 'https://github.com/Songmu/laminate/releases/download/v0.0.3/laminate_v0.0.3_darwin_amd64.zip'
+      sha256 '3ca04d51d8141422102b8113c64be68e8acad5a1675fdd39eafb25dccfb923a2'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/Songmu/laminate/releases/download/v0.0.2/laminate_v0.0.2_linux_arm64.tar.gz'
-      sha256 '0f011cdb07547a5a1ff16e934b851ce38c9885af53c6de8764db2c623b20ad07'
+      url 'https://github.com/Songmu/laminate/releases/download/v0.0.3/laminate_v0.0.3_linux_arm64.tar.gz'
+      sha256 '1f38d4bc8c956900bdd79c7d007634ea49120868bfa1a303818a1f3316dc25e3'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/Songmu/laminate/releases/download/v0.0.2/laminate_v0.0.2_linux_amd64.tar.gz'
-      sha256 '9c22d968b0ddb9667f37e6bf3a0f4864f25c86028992e597921ae29c02441ced'
+      url 'https://github.com/Songmu/laminate/releases/download/v0.0.3/laminate_v0.0.3_linux_amd64.tar.gz'
+      sha256 '1855c6b057e8a9d1e7edf975fc09bf56a14bc763c66ff6617ac4db67b234c0bf'
     end
   end
 
