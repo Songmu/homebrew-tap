ebdf366161c406ece376c0d600bec98a3a588d4a
Songmu <y.songmu@gmail.com>
2025-12-26 09:36:53 +0000

    update to newer version

--- Formula/gh2changelog.rb
+++ Formula/gh2changelog.rb
@@ -1,26 +1,26 @@
 class Gh2changelog < Formula
-  version '0.3.0'
+  version '0.4.0'
   homepage 'https://github.com/Songmu/gh2changelog'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/Songmu/gh2changelog/releases/download/v0.3.0/gh2changelog_v0.3.0_darwin_arm64.zip'
-      sha256 '9eee4c3282a57b3b1d4e2409fe284dfca68f601520b85dd8040a61ce0108976a'
+      url 'https://github.com/Songmu/gh2changelog/releases/download/v0.4.0/gh2changelog_v0.4.0_darwin_arm64.zip'
+      sha256 '3281071f0e3427f943c0a50840bd8ac824e840e2ea96b2d673bfe7ca2a126757'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/Songmu/gh2changelog/releases/download/v0.3.0/gh2changelog_v0.3.0_darwin_amd64.zip'
-      sha256 'a644f65aed14d0a5bceef58415ffb7a146676784db0b0ce0dd897dfad1e4bdcd'
+      url 'https://github.com/Songmu/gh2changelog/releases/download/v0.4.0/gh2changelog_v0.4.0_darwin_amd64.zip'
+      sha256 'c575cd8836699ee4997e4f2b7baceaa389395662e4b159070e0cf245c834bad8'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/Songmu/gh2changelog/releases/download/v0.3.0/gh2changelog_v0.3.0_linux_arm64.tar.gz'
-      sha256 '2bd738cfcacd5d35b4805b9942b09327f187a28895f7b4527e40fd84c96da1ab'
+      url 'https://github.com/Songmu/gh2changelog/releases/download/v0.4.0/gh2changelog_v0.4.0_linux_arm64.tar.gz'
+      sha256 'd484c90bb66ab124e37a13548d1d18982301ce8ddfa8b9136f15125c52d05e84'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/Songmu/gh2changelog/releases/download/v0.3.0/gh2changelog_v0.3.0_linux_amd64.tar.gz'
-      sha256 '2af1db4b2a4c0ebf13e010646423e263d233f811c101e0e0613f03eb7fe4aab5'
+      url 'https://github.com/Songmu/gh2changelog/releases/download/v0.4.0/gh2changelog_v0.4.0_linux_amd64.tar.gz'
+      sha256 '226c1185da2d68afb00d3a8569bd27c83b32b8afcfaa94e4b3b9185ac09ac314'
     end
   end
 
