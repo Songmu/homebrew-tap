f2b14ea0eb937a4171f1aa8d14392e7098873f8b
Songmu <y.songmu@gmail.com>
2026-03-19 00:56:23 +0000

    update to newer version

--- Formula/gitrail.rb
+++ Formula/gitrail.rb
@@ -1,27 +1,27 @@
 class Gitrail < Formula
-  version '0.0.2'
+  version '0.0.3'
   homepage 'https://github.com/Songmu/gitrail'
   license 'MIT'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/Songmu/gitrail/releases/download/v0.0.2/gitrail_v0.0.2_darwin_arm64.zip'
-      sha256 '1d3302fa1f7f2a9e606b45a74f64b21bfda2f4aa37651418f9e44023049094c5'
+      url 'https://github.com/Songmu/gitrail/releases/download/v0.0.3/gitrail_v0.0.3_darwin_arm64.zip'
+      sha256 '475603d13b3fbf22d34cbbbd564842b6a27b60a7103801818bb50af545de4bfd'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/Songmu/gitrail/releases/download/v0.0.2/gitrail_v0.0.2_darwin_amd64.zip'
-      sha256 '8423e4fb98a9ed8107830394bdf799048f63f72f42dcd0ecfaea1d4fb5bcb5d6'
+      url 'https://github.com/Songmu/gitrail/releases/download/v0.0.3/gitrail_v0.0.3_darwin_amd64.zip'
+      sha256 'd732d706a4168124162105adf5922f688d68e8ca0ee84a4c2fe78855519b1636'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/Songmu/gitrail/releases/download/v0.0.2/gitrail_v0.0.2_linux_arm64.tar.gz'
-      sha256 '506f3646ebc425cf53fd5230e676a4f1f9ac3453b28c155a1b53b65bfcb29636'
+      url 'https://github.com/Songmu/gitrail/releases/download/v0.0.3/gitrail_v0.0.3_linux_arm64.tar.gz'
+      sha256 'c462bc8f6206e44125b76e1c7efee78da6d7582be52ad28a765876e449401611'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/Songmu/gitrail/releases/download/v0.0.2/gitrail_v0.0.2_linux_amd64.tar.gz'
-      sha256 'b488adeaa2033552c48c137d461ab9a81e9f94164dc6ccc25039b8465c90b187'
+      url 'https://github.com/Songmu/gitrail/releases/download/v0.0.3/gitrail_v0.0.3_linux_amd64.tar.gz'
+      sha256 '2b0bb1c5e9a0cfca49fe574860b0e9571381f735599183ded93c4d7acf28a516'
     end
   end
 
