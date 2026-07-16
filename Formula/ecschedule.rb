1315f0a6cfd34e2db55b8c3c93be6771f3602695
Songmu <y.songmu@gmail.com>
2026-07-16 00:58:52 +0000

    update to newer version

--- Formula/ecschedule.rb
+++ Formula/ecschedule.rb
@@ -1,26 +1,26 @@
 class Ecschedule < Formula
-  version '0.19.0'
+  version '0.20.0'
   homepage 'https://github.com/Songmu/ecschedule'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/Songmu/ecschedule/releases/download/v0.19.0/ecschedule_v0.19.0_darwin_arm64.zip'
-      sha256 '8444b0a8aff03ff0f457c3ea14fcd99e7732c164aefd1a8fbb5889bad45026ef'
+      url 'https://github.com/Songmu/ecschedule/releases/download/v0.20.0/ecschedule_v0.20.0_darwin_arm64.zip'
+      sha256 '7c0a1041b7548bd38345d659065b7f0cff5c5d0111f9330c5d048ffcc804988b'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/Songmu/ecschedule/releases/download/v0.19.0/ecschedule_v0.19.0_darwin_amd64.zip'
-      sha256 'f5fba3f34021db29f05504ba9f0eaa035a2ce4390c52ca80889a4d7dc59ea2f8'
+      url 'https://github.com/Songmu/ecschedule/releases/download/v0.20.0/ecschedule_v0.20.0_darwin_amd64.zip'
+      sha256 '81a2b62502d5ce2874619f136516e432644420c240820a8c044ed65f3079ad5b'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/Songmu/ecschedule/releases/download/v0.19.0/ecschedule_v0.19.0_linux_arm64.tar.gz'
-      sha256 '2301ea9adf41b871bd3af88c3bff90995e032d9da1fb61665f27cb3e51340add'
+      url 'https://github.com/Songmu/ecschedule/releases/download/v0.20.0/ecschedule_v0.20.0_linux_arm64.tar.gz'
+      sha256 'd74a20758cf7be9f3add960b5960fe23ac5542be40c9a7b6089c450e5cffe7be'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/Songmu/ecschedule/releases/download/v0.19.0/ecschedule_v0.19.0_linux_amd64.tar.gz'
-      sha256 'd2296fcf4b7eeadc01c99b88fd72e265bf16ae5a30c6a0d8bf2f23fb334b9024'
+      url 'https://github.com/Songmu/ecschedule/releases/download/v0.20.0/ecschedule_v0.20.0_linux_amd64.tar.gz'
+      sha256 'd312d155768c8c32353ab4e1deb04c1c0c7ea404d4747f60dd48079ee1f887ad'
     end
   end
 
--- Formula/slidown.rb
+++ Formula/slidown.rb
@@ -1,27 +1,27 @@
 class Slidown < Formula
-  version '0.0.7'
+  version '0.0.8'
   homepage 'https://github.com/Songmu/slidown'
   license 'MIT'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/Songmu/slidown/releases/download/v0.0.7/slidown_v0.0.7_darwin_arm64.zip'
-      sha256 '0a98deac6ba112408ebf9f3e6e116be11afb273ecab3d03447b0f89cbe82997e'
+      url 'https://github.com/Songmu/slidown/releases/download/v0.0.8/slidown_v0.0.8_darwin_arm64.zip'
+      sha256 '8b3122887f7aea0a8392ac57f94fb9073168e21a4a81e601dd7d5fd6560822d2'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/Songmu/slidown/releases/download/v0.0.7/slidown_v0.0.7_darwin_amd64.zip'
-      sha256 '2738a6293a37cc8ba2cd964d572f5046beb7a6ec4c061c4dc0647f75d6efca7f'
+      url 'https://github.com/Songmu/slidown/releases/download/v0.0.8/slidown_v0.0.8_darwin_amd64.zip'
+      sha256 'd8f5b5a839eadd9914636feb055bc568acdfdbfd4c2899a184691fd72c7d730c'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/Songmu/slidown/releases/download/v0.0.7/slidown_v0.0.7_linux_arm64.tar.gz'
-      sha256 '8c0ee5c2cd35e793a2329e8ac2d0198ff748674a71de90b5386a02f1d8a5a7a4'
+      url 'https://github.com/Songmu/slidown/releases/download/v0.0.8/slidown_v0.0.8_linux_arm64.tar.gz'
+      sha256 'be230cb7a5f9a71b1e540295bafbae4e8bcdadb351115976f31c04d6b31fb80a'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/Songmu/slidown/releases/download/v0.0.7/slidown_v0.0.7_linux_amd64.tar.gz'
-      sha256 '56918d08ce67612c792e29e63855c1762bcae926c5557392f848c488cc39a67b'
+      url 'https://github.com/Songmu/slidown/releases/download/v0.0.8/slidown_v0.0.8_linux_amd64.tar.gz'
+      sha256 '06821359bbf22fb9df10dfc5cd9cfe9fb52d570e22092ebab6b16ae20a6f8054'
     end
   end
 
