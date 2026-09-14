dd80ad964bd75929f8fcde4a459f5ba2bda09d7e
Songmu <y.songmu@gmail.com>
2026-09-15 00:57:24 +0900

    udpate

--- Formula/rssnip.rb
+++ Formula/rssnip.rb
@@ -1,27 +1,27 @@
 class Rssnip < Formula
-  version '0.0.1'
+  version '0.0.2'
   homepage 'https://github.com/Songmu/rssnip'
   license 'MIT'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/Songmu/rssnip/releases/download/v0.0.1/rssnip_v0.0.1_darwin_arm64.zip'
-      sha256 'fa4055c1a9eecc0eee99e6c9d23061da6ac1f2e64690ce84bee9d978c14b5675'
+      url 'https://github.com/Songmu/rssnip/releases/download/v0.0.2/rssnip_v0.0.2_darwin_arm64.zip'
+      sha256 'f744ab981265ea7b03224c0c4715212078017bed3131d098708179f4ff4c131d'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/Songmu/rssnip/releases/download/v0.0.1/rssnip_v0.0.1_darwin_amd64.zip'
-      sha256 '54b128643f32b09e60c6dc934b2408b3b83599939c22808fced026985cbce5a3'
+      url 'https://github.com/Songmu/rssnip/releases/download/v0.0.2/rssnip_v0.0.2_darwin_amd64.zip'
+      sha256 'a530c60dcfb53bb4ab3dcfca1ee48297840a65224cc9a98246e263a284befb35'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/Songmu/rssnip/releases/download/v0.0.1/rssnip_v0.0.1_linux_arm64.tar.gz'
-      sha256 'c158795cebd90dc063b8d6210e9e38e6e14c9baddbd11f97b91bfbf2dfbab6ac'
+      url 'https://github.com/Songmu/rssnip/releases/download/v0.0.2/rssnip_v0.0.2_linux_arm64.tar.gz'
+      sha256 'd6e96edfe0135302b8fbe02132097b8f70e7bdf08808a0171d3d69172e8c65e3'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/Songmu/rssnip/releases/download/v0.0.1/rssnip_v0.0.1_linux_amd64.tar.gz'
-      sha256 '551cd25974e3eb609c90c60af8074b1f9d008fe23e40b582998b76ba05cb353f'
+      url 'https://github.com/Songmu/rssnip/releases/download/v0.0.2/rssnip_v0.0.2_linux_amd64.tar.gz'
+      sha256 '935a232b8915278769159c3521a2a046f48b166bbae72bcc16c441204075ab3a'
     end
   end
 
